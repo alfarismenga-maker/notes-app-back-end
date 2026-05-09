5f10e240cf9c8e8dde326b0b0a2ceb8c8b69a2b1
alfaris <alfarismenga@gmail.com>
2026-05-09 15:31:37 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -5,14 +5,17 @@ import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+// hanya untuk log saja
+const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
 
+app.use(express.json());
 app.use(cors({
   origin: '*'
 }));
 
 app.use('/', routes);
 
+// ⚠️ INI BAGIAN PALING PENTING
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running at http://${host}:${port}`);
 });
